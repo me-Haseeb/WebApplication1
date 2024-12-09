54b5c4b074a0ccf5afc40d2a24ea4034905a37d6
Student <Student@DESKTOP-M561BCP>
2024-12-09 20:33:28 +0500

    Add project files.

---
+++ WebApplication1/Controllers/MathController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class MathController : Controller
+    {
+        // GET: Math
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection obj)
+        {
+            try
+            {
+                int num1 = Convert.ToInt32(obj["txtnum1"]);
+                int num2 = Convert.ToInt32(obj["txtnum2"]);
+                string operation = obj["operation"];
+
+                string result = string.Empty;
+
+                switch (operation)
+                {
+                    case "Add":
+                        result = $"The sum is {num1 + num2}";
+                        break;
+
+                    case "Subtract":
+                        result = $"The difference is {num1 - num2}";
+                        break;
+
+                    case "Multiply":
+                        result = $"The product is {num1 * num2}";
+                        break;
+
+                    case "Divide":
+                        if (num2 != 0)
+                            result = $"The quotient is {num1 / num2}";
+                        else
+                            result = "Division by zero is not allowed.";
+                        break;
+
+                    default:
+                        result = "Invalid operation selected.";
+                        break;
+                }
+
+                ViewBag.data = result;
+            }
+            catch (Exception ex)
+            {
+                ViewBag.data = "Error: " + ex.Message;
+            }
+
+            return View();
+        }
+    }
+}
